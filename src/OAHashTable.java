ebae52f0ea6e590dc9c9bffc8beedc0cd2002bf3
tomerkatee <100376324+tomerkatee@users.noreply.github.com>
2022-06-04 12:23:07 +0300

    Update - removed rehashing
    Removed the rehashing feature comments and changed the way insert detects חזרה של מפתחות

--- src/OAHashTable.java
+++ src/OAHashTable.java
@@ -2,8 +2,6 @@ public abstract class OAHashTable implements IHashTable {
 	
 	private HashTableElement [] table;
 	private final HashTableElement DELETED = new HashTableElement(0, 0);
-	//private int deletedCount = 0;
-	private final double MAX_DELETED_PERCENT = 0.5;
 
 	public OAHashTable(int m) {
 		this.table = new HashTableElement[m];
@@ -30,34 +28,26 @@ public abstract class OAHashTable implements IHashTable {
 	
 	@Override
 	public void Insert(HashTableElement hte) throws TableIsFullException,KeyAlreadyExistsException {
-		if(Find(hte.GetKey()) != null){
-			throw new KeyAlreadyExistsException(hte);
-		}
 		for (int i = 0; i < table.length; i++) {
 			int probeIndex = Hash(hte.GetKey(), i);
-			if(table[probeIndex] == null || table[probeIndex] == DELETED){
-//				if(table[probeIndex] == DELETED){
-//					deletedCount--;
-//				}
+			if(table[probeIndex] == DELETED){
+				if(Find(hte.GetKey()) != null){
+					throw new KeyAlreadyExistsException(hte);
+				}
 				table[probeIndex] = hte;
 				return;
+			}
+			else if(table[probeIndex] == null){
+				table[probeIndex] = hte;
+				return;
+			}
+			else if(table[probeIndex].GetKey() == hte.GetKey()){
+				throw new KeyAlreadyExistsException(hte);
 			}
 		}
 		throw new TableIsFullException(hte);
 	}
-//	private void Rehash() {
-//		HashTableElement[] fullTable = table;
-//		table = new HashTableElement[table.length];
-//		for(HashTableElement element : fullTable){
-//			if(element != null && element != DELETED){
-//				try {
-//					Insert(element);
-//				} catch(Exception ignored) {
-//					return;
-//				}
-//			}
-//		}
-//	}
+
 	@Override
 	public void Delete(long key) throws KeyDoesntExistException {
 		for (int i = 0; i < table.length; i++) {
@@ -67,10 +57,6 @@ public abstract class OAHashTable implements IHashTable {
 			}
 			if(table[probeIndex].GetKey() == key && table[probeIndex] != DELETED){
 				table[probeIndex] = DELETED;
-//				if((++deletedCount/(float)table.length) > MAX_DELETED_PERCENT){
-//					//Rehash();
-//					deletedCount = 0;
-//				}
 				return;
 			}
 		}
